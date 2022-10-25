9421e587982b97d55b217663fa0b1c9568149fdf
Muhammad-AhsanTariq <106869223+Muhammad-AhsanTariq@users.noreply.github.com>
2022-10-26 01:21:36 +0500

    Update timeLock.sol

--- timeLock.sol
+++ timeLock.sol
@@ -3,56 +3,50 @@ pragma solidity ^0.8.4;
 
   import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
   import "@openzeppelin/contracts/access/Ownable.sol";
-  import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
   import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
-  contract MyToken is ERC20, Ownable {
+  contract timeLock is ERC20, Ownable {
  
    // calling SafeMath will add extra functions to the uint data type
     using SafeMath for uint; // you can make a call like myUint.add(123
     uint public releaseTime;
-    uint public lockedVal;
-    uint public mintVal;
+    uint public lockedAmount;
+    uint public mintAmount;
+    uint public transactionId;
   
-
-    struct bal{
+    struct userDetail{
+      uint mintAmnt;
       uint lockAmnt;
       uint time;
       address addr;
     }
 
-   mapping(address=>mapping(uint=>bool))public transfrMap;
-   mapping(address => mapping(uint=>bal)) public lockAcnt;
-   
+   mapping(uint=>userDetail)public User;
     
    error timeLimitNotComplete(); 
    
     constructor() ERC20("MyToken", "MTK") {}
-    uint public transactionId;
-    function mint( address to, uint val) public {
+    
+    function mint( address to, uint amount) public {
+      
       require( msg.sender != owner() , "Owner not allow");
        
-        lockedVal=val*30/100;
-        mintVal=val-lockedVal;
-         transactionId++;
-        releaseTime = block.timestamp.add(uint256(600)); 
+      lockedAmount=amount*30/100;
+      mintAmount=amount-lockedAmount;
+      transactionId++;
+      releaseTime = block.timestamp.add(uint256(600)); 
         
-          _mint(to,mintVal);
-          _mint(owner(),lockedVal);
-          lockAcnt[to][transactionId].time=releaseTime;
-          lockAcnt[to][transactionId].lockAmnt=lockedVal;
-          lockAcnt[to][transactionId].addr=to;
+      _mint(to,mintAmount);
+      _mint(owner(),lockedAmount);
+      User[transactionId]=userDetail(mintAmount,lockedAmount,releaseTime,to);
+  
     }
 
-    function withdraw(address to) public onlyOwner{
+    function withdraw(uint transactionID) public onlyOwner {
     
-     require(transfrMap[to][lockAcnt[to][transactionId].time]==false, "already transfered claim balance");
-     require(lockAcnt[to][transactionId].addr==to,"noTokenMinted");
+     if(block.timestamp >= User[transactionID].time){
 
-     if(block.timestamp >= lockAcnt[to][transactionId].time){
-
-      transfer(to,(lockAcnt[to][transactionId].lockAmnt) );
-      transfrMap[to][lockAcnt[to][transactionId].time]=true;
+      transfer(User[transactionID].addr,User[transactionID].lockAmnt); 
     } 
     else{
       revert timeLimitNotComplete(); 
