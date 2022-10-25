36dd2fe7d38fa340599c1dffb410fbc061fd4f8f
Muhammad-AhsanTariq <106869223+Muhammad-AhsanTariq@users.noreply.github.com>
2022-10-25 22:20:56 +0500

    Update timeLock.sol

--- timeLock.sol
+++ timeLock.sol
@@ -16,42 +16,43 @@ pragma solidity ^0.8.4;
   
 
     struct bal{
-      uint mintAmnt;
       uint lockAmnt;
       uint time;
       address addr;
     }
 
    mapping(address=>mapping(uint=>bool))public transfrMap;
-   mapping(address => bal) public lockAcnt;
+   mapping(address => mapping(uint=>bal)) public lockAcnt;
    
     
    error timeLimitNotComplete(); 
    
     constructor() ERC20("MyToken", "MTK") {}
-    
+    uint public transactionId;
     function mint( address to, uint val) public {
       require( msg.sender != owner() , "Owner not allow");
        
         lockedVal=val*30/100;
         mintVal=val-lockedVal;
-    
+         transactionId++;
         releaseTime = block.timestamp.add(uint256(600)); 
         
           _mint(to,mintVal);
           _mint(owner(),lockedVal);
-          lockAcnt[to]=bal(mintVal,lockedVal,releaseTime,to);    
+          lockAcnt[to][transactionId].time=releaseTime;
+          lockAcnt[to][transactionId].lockAmnt=lockedVal;
+          lockAcnt[to][transactionId].addr=to;
     }
 
     function withdraw(address to) public onlyOwner{
     
-     require(transfrMap[to][lockAcnt[to].time]==false, "already transfered claim balance");
-     require(lockAcnt[to].addr==to,"noTokenMinted");
+     require(transfrMap[to][lockAcnt[to][transactionId].time]==false, "already transfered claim balance");
+     require(lockAcnt[to][transactionId].addr==to,"noTokenMinted");
 
-     if(block.timestamp >= lockAcnt[to].time){
+     if(block.timestamp >= lockAcnt[to][transactionId].time){
 
-      transfer(to,(lockAcnt[to].lockAmnt) );
-      transfrMap[to][lockAcnt[to].time]=true;
+      transfer(to,(lockAcnt[to][transactionId].lockAmnt) );
+      transfrMap[to][lockAcnt[to][transactionId].time]=true;
     } 
     else{
       revert timeLimitNotComplete(); 
