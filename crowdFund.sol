c9f01fb81b7d2f7c87accf0c7b624d8a466155be
Muhammad-AhsanTariq <106869223+Muhammad-AhsanTariq@users.noreply.github.com>
2022-11-03 07:03:27 +0500

    Update crowdFund.sol

--- crowdFund.sol
+++ crowdFund.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+ contract crowdFund is ERC20,Ownable {
 
-contract MyToken is ERC20 {
-
-    IERC20 public to;
-    constructor(address _to) ERC20("MyToken", "MTK") {
-      to = IERC20(_to);
+   
+    constructor() ERC20("MyToken", "MTK") {
+          
+        pledgeAddr = (address(this));
+   
     }
     
-
  struct Campaign {
 
         address creator;
@@ -22,29 +22,36 @@ contract MyToken is ERC20 {
         uint endAt;
         bool claimed;
     }
+  struct user{
+
+      address donor;
+      address to;
+  }
 
     uint public _startAt;
     uint public _endAt;
-    uint public count;
+    uint public campaignId;
+    address public pledgeAddr;
+    //address public tokenAddr= address(this);
+  
     // Mapping from id to Campaign
     mapping(uint => Campaign) public campaigns;
     // Mapping from campaign id => pledger => amount pledged
     mapping(uint => mapping(address => uint)) public pledgedAmount;
-
-   function mint(address To, uint256 amount) public  {
-        _mint(To, amount);
+    mapping(uint => user)public _address;
+   
+   function mint( address to,uint256 amount) public{
+        _mint(to, amount);
     }
 
     function launch(
         uint _goal
-       ) external {
+       ) public onlyOwner {
+
         _startAt = block.timestamp;
         _endAt = block.timestamp+uint(600);
-        require(_endAt >= _startAt, "end at > start at");
-        
-
-        count += 1;
-        campaigns[count] = Campaign({
+        campaignId++;
+        campaigns[campaignId] = Campaign({
             creator: msg.sender,
             goal: _goal,
             pledged: 0,
@@ -55,39 +62,37 @@ contract MyToken is ERC20 {
 
     }
 
-    function cancel(uint _id) external {
-        Campaign memory campaign = campaigns[_id];
-        require(campaign.creator == msg.sender, "not creator");
-        require(block.timestamp <= campaign.startAt, "started");
-
-        delete campaigns[_id];
-       
-    }
-
-    function pledge(uint _id, uint _amount) external  {
+    function pledges(address from,uint _id, uint _amount) public  {
         Campaign storage campaign = campaigns[_id];
         require(block.timestamp >= campaign.startAt, "not started");
         require(block.timestamp <= campaign.endAt, "ended");
-
+         
         campaign.pledged += _amount;
-        pledgedAmount[_id][msg.sender] += _amount;
-        to.transferFrom(msg.sender, address(this), _amount);
+      
+      
+        pledgedAmount[_id][from] += _amount;
+       _address[_id] = user(from,pledgeAddr);
+       
 
-        
+       _transfer(from,pledgeAddr,_amount);
+    
     }
 
-    function unpledge(uint _id, uint _amount) external {
+    function unpledge(uint _id, uint _amount) public {
         Campaign storage campaign = campaigns[_id];
         require(block.timestamp <= campaign.endAt, "ended");
-
+         address to=  _address[_id].donor;
+       
         campaign.pledged -= _amount;
-        pledgedAmount[_id][msg.sender] -= _amount;
-        to.transfer(msg.sender, _amount);
+        pledgedAmount[_id][to] -= _amount;
+       
+      
+        _transfer(pledgeAddr,to, _amount);
 
        
     }
 
-    function claim(uint _id) external {
+    function claim(uint _id) public {
         Campaign storage campaign = campaigns[_id];
         require(campaign.creator == msg.sender, "not creator");
         require(block.timestamp >= campaign.endAt, "not ended");
@@ -95,19 +100,30 @@ contract MyToken is ERC20 {
         require(!campaign.claimed, "claimed");
 
         campaign.claimed = true;
-        to.transfer(campaign.creator, campaign.pledged);
+        _transfer(pledgeAddr,campaign.creator, campaign.pledged);
 
     
     }
 
-    function refund(uint _id) external {
+      function cancel(uint _id) public onlyOwner {
         Campaign memory campaign = campaigns[_id];
-        require(block.timestamp > campaign.endAt, "not ended");
-        require(campaign.pledged < campaign.goal, "pledged >= goal");
+        require(campaign.creator == msg.sender, "not creator");
+        require(block.timestamp >= campaign.startAt, "started");
 
-        uint bal = pledgedAmount[_id][msg.sender];
-        pledgedAmount[_id][msg.sender] = 0;
-        to.transfer(msg.sender, bal);
+        delete campaigns[_id];
+       
+    }
+
+    function refund(uint _id) public {
+        Campaign memory campaign = campaigns[_id];
+        require(block.timestamp >= campaign.endAt, "not ended");
+        require(campaign.pledged < campaign.goal, "pledged >= goal");
+        
+        address to=  _address[_id].donor;
+        uint bal = pledgedAmount[_id][to];
+        pledgedAmount[_id][to] = 0;
+         
+        _transfer(pledgeAddr,to, bal);
 
       
     }
